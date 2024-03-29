e1198dea078fac109fa143aef442d7f9917f23b3
Alex Garbarino <alexiothebrave@gmail.com>
2024-03-28 21:17:04 -0500

    First one done

--- problems/01-keys-in-object.js
+++ problems/01-keys-in-object.js
@@ -6,15 +6,25 @@ Do this once using using a `for...in` loop and once using `Object.keys`.
 
 Examples:
 
+***********************************************************************/
 let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
 let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
 keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
-***********************************************************************/
 
+
+//************for...in loop
 function keysInObject(obj) {
-  console.log("what's up Manni?")
-  console.log("Hello Alex")
+  let keys = [];
+  for (let item in obj) {
+    keys.push(item);
+  }
+  console.log(keys);
+}
+//*********Object.keys()*********
+
+function keysInObject(obj){
+  console.log(Object.keys(obj));
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
