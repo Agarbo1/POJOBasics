30a7150e3caacd55a310efe778e2f559c38859c4
Manni <emmanuelmelgoza1@gmail.com>
2024-03-28 20:57:31 -0600

    problem 4 solved/nav:Alex/driver:Manni

--- problems/04-get-full-name.js
+++ problems/04-get-full-name.js
@@ -4,15 +4,21 @@ and returns a string containing their full name.
 
 
 Examples:
+***********************************************************************/
+
+function getFullName(person) {
+  // Your code here
+  // let fullName = person.firstName + ' ' + person.lastName;
+  let fullName = person['firstName'] + ' ' + person['lastName'];
+  return fullName;
+
+
+}
+
 let p1 = {firstName: 'John', lastName: 'Doe'};
 getFullName(p1); // => 'John Doe'
 let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
 getFullName(p2); // => 'Charlie Brown'
-***********************************************************************/
-
-function getFullName(person) {
-  // Your code here 
-}
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = getFullName;
